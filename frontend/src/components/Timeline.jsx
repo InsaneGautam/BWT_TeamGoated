4f391d2874851d7940d7b0afb95a02aadac8e057
Gautam Naik <07naikgautam@gmail.com>
2026-03-04 15:06:34 +0530

    Update Timeline.jsx

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
-const Timeline = ({ employeeId }) => {
-  const [events, setEvents] = useState([]);
+function Timeline(){
 
-  useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/events/${employeeId}`)
-      .then(res => setEvents(res.data))
-      .catch(err => console.error(err));
-  }, [employeeId]);
+  return(
 
-  return (
-    <div>
-      <h2>Activity Timeline</h2>
+    <div style={{
+      background:"#1e293b",
+      padding:"20px",
+      borderRadius:"10px"
+    }}>
+
+      <h3>Activity Timeline</h3>
+
       <ul>
-        {events.map((event, index) => (
-          <li key={index}>
-            {event.timestamp} - {event.event_type}
-          </li>
-        ))}
+        <li>10:00 Login</li>
+        <li>10:10 File Access</li>
+        <li>23:30 Data Download</li>
       </ul>
+
     </div>
+
   );
-};
+
+}
 
 export default Timeline;
