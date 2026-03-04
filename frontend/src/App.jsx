b134c6b246fe4bb21703eb20edb82b50fe061f65
Nandith M Y <nandinandith@gmail.com>
2026-03-04 14:24:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Dashboard from "./pages/Dashboard";
-import Alerts from "./pages/Alerts";
-import EmployeeProfile from "./pages/EmployeeProfile";
-import Investigation from "./pages/Investigation";
 
 function App() {
-
-  return (
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route path="/" element={<Dashboard />} />
-
-        <Route path="/alerts" element={<Alerts />} />
-
-        <Route path="/employee" element={<EmployeeProfile />} />
-
-        <Route path="/investigation" element={<Investigation />} />
-
-      </Routes>
-
-    </BrowserRouter>
-
-  );
-
+  return <Dashboard />;
 }
 
 export default App;
