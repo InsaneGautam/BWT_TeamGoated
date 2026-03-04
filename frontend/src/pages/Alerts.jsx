7159f1889852d0abe4c6d6c9edb0068d624659f4
Gautam Naik <07naikgautam@gmail.com>
2026-03-04 15:03:59 +0530

    Update Alerts.jsx

--- frontend/src/pages/Alerts.jsx
+++ frontend/src/pages/Alerts.jsx
@@ -1,12 +1,19 @@
+import React from "react";
 import AlertFeed from "../components/AlertFeed";
 
-const Alerts = () => {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Alerts</h1>
-      <AlertFeed />
+function Alerts(){
+
+  return(
+
+    <div style={{padding:"20px"}}>
+
+      <h2>Security Alerts</h2>
+
+      <AlertFeed/>
+
     </div>
+
   );
-};
+}
 
 export default Alerts;
