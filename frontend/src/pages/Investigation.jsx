b989b27f28a54669c895551abcf0ef4a6adca8a6
Nandith M Y <nandinandith@gmail.com>
2026-03-04 13:52:52 +0530

    Update Investigation.jsx

---
+++ frontend/src/pages/Investigation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Investigation(){
+
+  return(
+
+    <div style={{padding:"30px"}}>
+
+      <h2>Investigation Details</h2>
+
+      <p>AI explanation of suspicious activities will appear here.</p>
+
+    </div>
+
+  )
+
+}
