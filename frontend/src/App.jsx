7932daf16c945d15d3c3c3faef8cfcacdf7afc53
Gautam Naik <07naikgautam@gmail.com>
2026-03-04 15:02:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import Dashboard from "./pages/Dashboard.jsx";
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+import Alerts from "./pages/Alerts";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div style={{background:"#0f172a", minHeight:"100vh", color:"white"}}>
+      
+      <h1 style={{textAlign:"center", padding:"20px"}}>
+        AI Insider Threat Detection System
+      </h1>
+
+      <Dashboard />
+      <Alerts />
+
+    </div>
+  );
 }
 
 export default App;
