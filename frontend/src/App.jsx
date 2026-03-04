60e8f4b22df409048bbc66876c69a4aea08cb36c
NISHAN-HS <nishan77779@gmail.com>
2026-03-04 14:42:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import EmployeeProfile from "./pages/EmployeeProfile";
+import Alerts from "./pages/Alerts";
+import Investigation from "./pages/Investigation";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/employee/:id" element={<EmployeeProfile />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/investigation" element={<Investigation />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
