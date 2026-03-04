7897dbc22c67e53f937befef0e22b9cc015ff74c
Nandith M Y <nandinandith@gmail.com>
2026-03-04 13:52:12 +0530

    Update Alerts.jsx

---
+++ frontend/src/pages/Alerts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Alerts(){
+
+  return(
+
+    <div style={{padding:"30px"}}>
+
+      <h2>Alert Investigation Panel</h2>
+
+      <p>Select an alert to investigate suspicious behavior.</p>
+
+    </div>
+
+  )
+
+}
